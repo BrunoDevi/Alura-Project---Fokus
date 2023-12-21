56e6d3a626d459619b2dd9e6796015ff28b1bad2
Bruno Baêta <brunoantonio25@outlook.com>
2023-12-21 11:21:51 -0300

    FokusMode changing

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-const fokusMode = document.querySelectorAll('.app__card-button');
-console.log(fokusMode.length);
-console.log("uai");
+const html = document.querySelector('html');
+const btnLista = document.querySelectorAll('.app__card-button');
+const appMode = ['foco', 'descanso-curto', 'descanso-longo'];
+const bannerText1 = ['Otimize sua produtividade,', 'Que tal dar uma respirada?', 'Hora de voltar à superfície.'];
+const bannerText2 = ['mergulhe no que importa', 'Faça uma pausa curta!', 'Faça uma pausa longa.'];
+const musicaCheckBox = document.getElementById('alternar-musica');
+const musica = new Audio('/sons/Luna Rise, Part One.mp3');
+musica.loop = true;
+musica.volume = 0.5;
+
+let imagem = document.querySelector('.app__image');
+let primeiroTexto = document.querySelector('.app__title').childNodes[0];
+let segundoTexto = document.querySelector('.app__title-strong');
+
+for(let i = 0; i < btnLista.length; i++){
+    btnLista[i].addEventListener('click', ()=>{
+        html.setAttribute('data-contexto', appMode[i]);
+        imagem.setAttribute('src', `/imagens/${appMode[i]}.png`);
+
+        primeiroTexto.textContent = bannerText1[i];
+        segundoTexto.textContent = bannerText2[i];
+    })
+
+    btnLista[i].addEventListener('focus', () => {
+        for (let j = 0; j < btnLista.length; j++) {
+            btnLista[j].classList.remove('active');
+        }
+        btnLista[i].classList.add('active');
+    })
+
+}
+
+musicaCheckBox.addEventListener('change', function() {
+    if (this.checked) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+});
