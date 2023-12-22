2b9f2e3aac8a1066cdf4c4dc52363169191de6e5
Bruno Baêta <brunoantonio25@outlook.com>
2023-12-22 14:26:00 -0300

    code formating

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const html = document.querySelector('html');
 const btnLista = document.querySelectorAll('.app__card-button');
 const appMode = ['foco', 'descanso-curto', 'descanso-longo'];
-const bannerText1 = ['Otimize sua produtividade,', 'Que tal dar uma respirada?', 'Hora de voltar à superfície.'];
-const bannerText2 = ['mergulhe no que importa', 'Faça uma pausa curta!', 'Faça uma pausa longa.'];
 
 //BANNER Start
+const bannerText1 = ['Otimize sua produtividade,', 'Que tal dar uma respirada?', 'Hora de voltar à superfície.'];
+const bannerText2 = ['mergulhe no que importa', 'Faça uma pausa curta!', 'Faça uma pausa longa.'];
 let imagem = document.querySelector('.app__image');
 let primeiroTexto = document.querySelector('.app__title').childNodes[0];
 let segundoTexto = document.querySelector('.app__title-strong');
@@ -51,45 +51,43 @@ const playSound = new Audio('/sons/play.wav');
 const pauseSound = new Audio('/sons/pause.mp3');
 const alarmSound = new Audio ('/sons/beep.mp3');
 const telaDeTempo = document.querySelector('#timer');
-
 let temporizador = null;
 let contador = 1500;
 
-startPause.addEventListener('click', start); //Executa função Start ao clicar
+startPause.addEventListener('click', start); // Executa função Start ao clicar
 
 const contagemRegresiva = ()=>{
-    if(contador <= 0){ //Executa ao final da contagem
+    if(contador <= 0){ // Executa ao final da contagem
         alarmSound.play();
         zerar();
         displayTime()
         startPause.innerHTML = `<img class="app__card-primary-butto-icon" src="/imagens/play_arrow.png" alt=""/> <span id="start-pause__text">Começar</span>`;
-        alert('tempo encerrado');
         return
     }
 
-    contador -= 1; //Loop do temporizador
+    contador -= 1; // Loop do temporizador
     displayTime()
 }
 
 function start(){
-    if(temporizador){ //executa ao Pausar
+    if(temporizador){ // Executa ao Pausar
         zerar();
         startPause.innerHTML = `<img class="app__card-primary-butto-icon" src="/imagens/play_arrow.png" alt=""/> <span id="start-pause__text">Começar</span>`;
         pauseSound.play();
         return
     }
 
-    temporizador = setInterval(contagemRegresiva, 1000); //Executa ao iniciar
+    temporizador = setInterval(contagemRegresiva, 1000); // Executa ao iniciar
     startPause.innerHTML = `<img class="app__card-primary-butto-icon" src="/imagens/pause.png" alt=""/> <span id="start-pause__text">Pausar</span>`;
     playSound.play();
 }
 
-function zerar(){ //Para o temporizado porem mantem o valor do contador
+function zerar(){ // Para o temporizado porem mantem o valor do contador
     clearInterval(temporizador);
     temporizador = null;
 }
 
-function atualizarMaxTime(indice){ //Define o contador de acordo com o appMode
+function atualizarMaxTime(indice){ // Define o contador de acordo com o appMode
     switch (appMode[indice]) {
         case 'foco':
             return 1500
@@ -108,6 +106,6 @@ function displayTime(){ // Mostra o contador na tela
     telaDeTempo.innerHTML = `${tempoFormatado}`;
 }
 
-displayTime() //Mantem o contador sempre exibido na tela
+displayTime() // Mantem o contador sempre exibido na tela
 
 //TEMPORIZADOR End
